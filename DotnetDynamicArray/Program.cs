f6353805ea3c7cc0322b597569bb506a6e54a648
sim511777 <sim51177@gmail.com>
2020-10-19 14:29:04 +0900

    DotnetDynamicArray 추가

---
+++ DotnetDynamicArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetDynamicArray {
+    class Program {
+        static void Main(string[] args) {
+            List<int> a = new List<int>();
+            for (int i = 0; i <= 17; i++) {
+                a.Add(i);
+                Console.WriteLine("{0}: {1}", i, a.Capacity);
+            }
+        }
+    }
+}
