a2ab9b094f67955fc192f0efce5f0d8347485209
sim511777 <sim51177@gmail.com>
2020-10-19 14:25:14 +0900

    DynamicArray_0 추가

--- DynamicArray/Program.cs
+++ DynamicArray/Program.cs
@@ -5,6 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DynamicArray {
+    public class DynamicArray_0 {
+        private object[] arr = new object[0];
+        
+        public void Add(object element) {
+            var temp = new object[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++) {
+                temp[i] = arr[i];
+            }
+            arr = temp;
+            arr[arr.Length - 1] = element;
+        }
+    }
+
     public class DynamicArray {
         private object[] arr;
         private const int GROWTH_FACTOR = 2;
