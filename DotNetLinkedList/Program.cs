587026fbd262a7a53c91d7a2d996c614ee3c9e98
sim511777 <sim51177@gmail.com>
2020-10-19 14:20:14 +0900

    DotNetLinkedList 추가

---
+++ DotNetLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLinkedList {
+    class Program {
+        static void Main(string[] args) {
+            var list = new LinkedList<string>();
+            list.AddLast("Apple");
+            list.AddLast("Banana");
+            list.AddLast("Lemon");
+
+            var node = list.Find("Banana");
+            var newNode = new LinkedListNode<string>("Grape");
+            list.AddAfter(node, newNode);
+
+            list.ToList().ForEach(p => Console.WriteLine(p));
+
+            list.Remove("Grape");
+
+            foreach (var m in list) {
+                Console.WriteLine(m);
+            }
+        }
+    }
+}
