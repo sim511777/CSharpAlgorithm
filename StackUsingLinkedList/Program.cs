ee2080a73b7af4c8c95d72c4c5415c2484a3c436
sim511777 <sim51177@gmail.com>
2020-10-26 02:17:10 +0900

    StackUsingLinkedList 추가

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedList {
+    // 단순화를 위해 object 데이타 타입 사용
+    public class StackUsingLinkedList {
+        private Node top = null;
+
+        public void Push(object data) {
+            if (top == null) {
+                top = new Node(data);
+            } else {
+                // 노드추가
+                var node = new Node(data);
+                node.Next = top;
+                top = node;
+            }
+        }
+
+        public object Pop() {
+            if (this.IsEmpty) {
+                throw new ApplicationException("Empty");
+            }
+
+            object data = top.Data;
+            top = top.Next;
+            return data;
+        }
+
+        public object Peek() {
+            if (this.IsEmpty) {
+                throw new ApplicationException("Empty");
+            }
+
+            return top.Data;
+        }
+
+        public bool IsEmpty {
+            get { return top == null; }
+        }
+
+        // StackUsingLinkedList 클래스에서만 
+        // 사용하는 Node 클래스
+        private class Node {
+            public object Data { get; set; }
+            public Node Next { get; set; }
+            public Node(object data) {
+                Data = data;
+                Next = null;
+            }
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
