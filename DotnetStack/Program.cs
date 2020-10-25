ad8c1f7433524720684cbdd0321903e19ba19fbf
sim511777 <sim51177@gmail.com>
2020-10-26 02:21:04 +0900

    DotnetStack 추가

---
+++ DotnetStack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetStack {
+    class Program {
+        static void Main(string[] args) {
+            Stack<double> s = new Stack<double>();
+            s.Push(1.25);
+            s.Push(2.50);
+            s.Push(3.75);
+
+            double val = s.Pop();
+            Console.WriteLine(val);
+        }
+    }
+}
